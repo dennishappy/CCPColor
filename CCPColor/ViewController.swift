0f8fa007945d8297675d3e7333aea530063983ad
Brian.Chu <chuchengpeng0119@163.com>
2017-02-16 11:25:12 +0800

    Initial Commit

---
+++ CCPColor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CCPColor
+//
+//  Created by 储诚鹏 on 17/2/16.
+//  Copyright © 2017年 chengpeng.chu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
